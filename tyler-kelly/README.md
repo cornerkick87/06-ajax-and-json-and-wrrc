e03131451bb6f59ecae5ebbb2b70f562d34ee76d
Tyler Pearson <tylerthewonderfull@gmail.com>
2018-06-12 12:10:10 -0700

    finished project. Blog is now pulling from database if it fins the local storage empty.

---
+++ tyler-kelly/README.md
@@ -0,0 +1,23 @@
+# AJAX and JSON and WRRC
+
+**Author**: Tyler Pearson && Bennett
+**Version**: 1.0.0 
+
+## Overview
+A blog with the ability to pull article data from a database if the user does not have the data in local stoage already 
+
+## Getting Started
+Navigate to desired directory
+Git clone the repo 
+Open in browser
+
+## Architecture
+Built using HTML, CSS, JS qith JQUERY and Ajax.
+
+## Change Log
+
+
+06-012-2018 12:09pm - Application now pulls data from a database if local storage is not found
+
+## Credits and Collaborations
+
--- tyler-kelly/scripts/article.js
+++ tyler-kelly/scripts/article.js
@@ -1,6 +1,6 @@
 'use strict';
 
-function Article (rawDataObj) {
+function Article(rawDataObj) {
   this.author = rawDataObj.author;
   this.authorUrl = rawDataObj.authorUrl;
   this.title = rawDataObj.title;
@@ -12,16 +12,16 @@ function Article (rawDataObj) {
 // REVIEW: Instead of a global `articles = []` array, let's attach this list of all articles directly to the constructor function. Note: it is NOT on the prototype. In JavaScript, functions are themselves objects, which means we can add properties/values to them at any time. In this case, the array relates to ALL of the Article objects, so it does not belong on the prototype, as that would only be relevant to a single instantiated Article.
 Article.all = [];
 
-// COMMENT: Why isn't this method written as an arrow function?
-// PUT YOUR RESPONSE HERE
-Article.prototype.toHtml = function() {
+// DONE: Why isn't this method written as an arrow function?
+// This functions requires the use of contexual 'this', which an arrow fuinction cannot use.
+Article.prototype.toHtml = function () {
   let template = Handlebars.compile($('#article-template').text());
 
-  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn)) / 60 / 60 / 24 / 1000);
 
-  // COMMENT: What is going on in the line below? What do the question mark and colon represent? How have we seen this same logic represented previously?
+  // Done COMMENT: What is going on in the line below? What do the question mark and colon represent? How have we seen this same logic represented previously?
   // Not sure? Check the docs!
-  // PUT YOUR RESPONSE HERE
+  //RESPONSE : The question mark in this line of code is asking for the published date. The colon is for seperating the published date and if it is a daft or not.  
   this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
   this.body = marked(this.body);
 
@@ -33,11 +33,12 @@ Article.prototype.toHtml = function() {
 // REVIEW: This function will take the rawData, how ever it is provided, and use it to instantiate all the articles. This code is moved from elsewhere, and encapsulated in a simply-named function for clarity.
 
 // COMMENT: Where is this function called? What does 'rawData' represent now? How is this different from previous labs?
-// PUT YOUR RESPONSE HERE
+//RESPONSE: 
 Article.loadAll = articleData => {
-  articleData.sort((a,b) => (new Date(b.publishedOn)) - (new Date(a.publishedOn)))
+  articleData.sort((a, b) => (new Date(b.publishedOn)) - (new Date(a.publishedOn)))
 
   articleData.forEach(articleObject => Article.all.push(new Article(articleObject)))
+  articleView.initIndexPage();
 }
 
 // REVIEW: This function will retrieve the data from either a local or remote source, and process it, then hand off control to the View.
@@ -45,9 +46,30 @@ Article.fetchAll = () => {
   // REVIEW: What is this 'if' statement checking for? Where was the rawData set to local storage?
   if (localStorage.rawData) {
 
-    Article.loadAll();
+    console.log('loaded local storage');
+    let localArr = JSON.parse(localStorage.rawData)
+    console.log(localArr);
+    Article.loadAll(localArr);
+
+
+
 
   } else {
+    $.ajax({
+      url: 'data/hackeripsum.json',
+      headers: {},
+      success: function (data, message, xhr) {
+        console.log(data);
+        localStorage.setItem('rawData', JSON.stringify(data))
+        console.log(Article.all);
+        Article.loadAll(data);
+      }
+    })
 
   }
+
 }
+
+
+
+
